a352413bbb95ad4f696ab2355ce034489e0e38a0
Deathray <acn3263827@gmail.com>
2025-01-25 15:42:45 -0600

    Item_box please dont fuck things up. GIT is fragile

---
+++ bubblesumo/Scenes/bounce_platform.gd
@@ -0,0 +1,15 @@
+extends StaticBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func FIRE(ob:Node3D) -> void:
+	if ob.has_method("apply_impulse"):
+		ob.apply_impulse(Vector3(0, 30, 0))
